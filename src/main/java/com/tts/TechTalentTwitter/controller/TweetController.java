93366bec2d9d20eb0e94a1b49b03d32cd7ffb2f2
Joey Whalen <jtktwhalen@gmail.com>
2021-03-03 21:32:10 -0500

    Updated TweetController

--- src/main/java/com/tts/TechTalentTwitter/controller/TweetController.java
+++ src/main/java/com/tts/TechTalentTwitter/controller/TweetController.java
@@ -3,6 +3,7 @@ package com.tts.TechTalentTwitter.controller;
 import com.tts.TechTalentTwitter.model.Tweet;
 import com.tts.TechTalentTwitter.model.TweetDisplay;
 import com.tts.TechTalentTwitter.model.User;
+import com.tts.TechTalentTwitter.repository.TagRepository;
 import com.tts.TechTalentTwitter.service.TweetService;
 import com.tts.TechTalentTwitter.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -24,9 +27,27 @@ public class TweetController {
   @Autowired
   private TweetService tweetService;
 
+  @Autowired
+  private TagRepository tagRepository;
+
   @GetMapping(value = { "/tweets", "/" })
-  public String getFeed(Model model) {
-    List<TweetDisplay> tweets = tweetService.findAll();
+  public String getFeed(@RequestParam(value = "filter", required = false) String filter, Model model){
+    User loggedInUser = userService.getLoggedInUser();
+    List<TweetDisplay> tweets = new ArrayList<>();
+
+    if (filter == null) {
+      filter = "all";
+    }
+    if (filter.equalsIgnoreCase("following")) {
+      List<User> following = loggedInUser.getFollowing();
+      tweets = tweetService.findAllByUsers(following);
+      model.addAttribute("filter", "following");
+    }
+    else {
+      tweets = tweetService.findAll();
+      model.addAttribute("filter", "all");
+    }
+
     model.addAttribute("tweetList", tweets);
     return "feed";
   }
